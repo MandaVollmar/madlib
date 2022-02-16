07f7e56dcafb2e748ef29bf132ffdd1968eb80ef
Amanda Vollmar <mandavollmar@Amandas-MBP.hsd1.co.comcast.net>
2022-02-16 15:39:37 -0700

    Arrays completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Create 3 size 5 arrays: nouns, adjectives, and verbs
+        String[] noun = {"book", "banana", "wand", "coffee", "kazoo"};
+        String[] adjective ={"furry", "mental", "illegal", "massive", "raw"};
+        String[] verb ={"read", "eat", "shop", "knock", "die"};
+
+    //get a random number from the range of zero to the number
+        // passed in as a parameter.
+
+        /* Write a small story that randomly gets words from each array to
+        fill in blank points in the story based on the word type.
+         */
+        /*
+         */
     }
 }
