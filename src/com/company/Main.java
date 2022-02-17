11de987e8916bb0f0be1e92c5aa10e2379465927
Amanda Vollmar <mandavollmar@Amandas-MBP.hsd1.co.comcast.net>
2022-02-16 17:20:15 -0700

    Start story

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -11,8 +10,8 @@ public class Main {
         String[] adjective ={"furry", "mental", "illegal", "massive", "raw"};
         String[] verb ={"read", "eat", "shop", "knock", "die"};
 
-    //get a random number from the range of zero to the number
-        // passed in as a parameter.
+    /*get a random number from the range of zero to the number
+        passed in as a parameter. */
         Random word = new Random();
         int indexNoun = word.nextInt(noun.length);
         Random description = new Random();
@@ -23,9 +22,10 @@ public class Main {
         /* Write a small story that randomly gets words from each array to
         fill in blank points in the story based on the word type.
          */
-        /*
-         */
-        System.out.println("noun " + noun[indexNoun] + " adjective " + adjective[indexAdjective]
-                + " verb " + verb[indexVerb]);
+        System.out.println("Learning to " + verb[indexVerb]+ " can be a(n) " +adjective[indexAdjective]+
+         " challenge." +"\n"+ "There is so much information to " +verb[indexVerb]+ " and retain." +
+                "\n" + "Although there are a lot of " +noun[indexNoun]+"s, it can be " +adjective[indexAdjective]+
+                " to know when to use them." +"\n");
+
     }
 }
